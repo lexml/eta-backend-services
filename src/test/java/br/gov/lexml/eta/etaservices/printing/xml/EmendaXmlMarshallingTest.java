955e882bc47f72a77d46bc9c365a9e87db9c0522
Ricardo da Silva Lima <rslima@gmail.com>
2022-07-22 14:18:16 -0300

    feat: Gera Pojos para JSON
    Teste JSON/FO funcionando

--- src/test/java/br/gov/lexml/eta/etaservices/printing/xml/EmendaXmlMarshallingTest.java
+++ src/test/java/br/gov/lexml/eta/etaservices/printing/xml/EmendaXmlMarshallingTest.java
@@ -1,19 +1,7 @@
 package br.gov.lexml.eta.etaservices.printing.xml;
 
 
-import br.gov.lexml.eta.etaservices.printing.AutoriaRecord;
-import br.gov.lexml.eta.etaservices.printing.ColegiadoApreciadorRecord;
-import br.gov.lexml.eta.etaservices.printing.ComandoEmendaRecord;
-import br.gov.lexml.eta.etaservices.printing.ComponenteEmendadoRecord;
-import br.gov.lexml.eta.etaservices.printing.DispositivoEmendaAdicionadoRecord;
-import br.gov.lexml.eta.etaservices.printing.DispositivosEmendaRecord;
 import br.gov.lexml.eta.etaservices.printing.Emenda;
-import br.gov.lexml.eta.etaservices.printing.EmendaRecord;
-import br.gov.lexml.eta.etaservices.printing.EpigrafeRecord;
-import br.gov.lexml.eta.etaservices.printing.ModoEdicaoEmenda;
-import br.gov.lexml.eta.etaservices.printing.OpcoesImpressaoRecord;
-import br.gov.lexml.eta.etaservices.printing.ParlamentarRecord;
-import br.gov.lexml.eta.etaservices.printing.RefProposicaoEmendadaRecord;
 import br.gov.lexml.eta.etaservices.printing.json.ArquivoEmenda;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -26,16 +14,7 @@ import javax.xml.transform.Source;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.util.Collections;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
-import static br.gov.lexml.eta.etaservices.printing.Sexo.M;
-import static br.gov.lexml.eta.etaservices.printing.SiglaCasaLegislativa.CD;
-import static br.gov.lexml.eta.etaservices.printing.TipoAutoria.PARLAMENTAR;
-import static br.gov.lexml.eta.etaservices.printing.TipoColegiado.PLENARIO;
 import static org.xmlunit.assertj3.XmlAssert.assertThat;
 
 class EmendaXmlMarshallingTest {
@@ -65,7 +44,7 @@ class EmendaXmlMarshallingTest {
                 .isEqualToIgnoringCase("cd");
     }
 
-    private EmendaRecord setupEmenda() {
+    private void setupEmenda() {
 
         ClassLoader classLoader = getClass().getClassLoader();
         try {
