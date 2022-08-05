af3e0a143e8cab38c920a0463c92f65aafc4b123
Ricardo da Silva Lima <rslima@gmail.com>
2022-08-05 08:05:46 -0300

    WIP - Adiciona funções para facilitar a geração das assinaturas

--- src/main/java/br/gov/lexml/eta/etaservices/printing/Autoria.java
+++ src/main/java/br/gov/lexml/eta/etaservices/printing/Autoria.java
@@ -1,6 +1,10 @@
 package br.gov.lexml.eta.etaservices.printing;
 
+import io.vavr.Tuple2;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public interface Autoria {
     TipoAutoria getTipo();
@@ -9,18 +13,35 @@ public interface Autoria {
 
     int getQuantidadeAssinaturasAdicionaisSenadores();
 
+    @SuppressWarnings("unused")
+    default int[] getAssinaturasAdicionaisSenadores() {
+        return IntStream.range(0, getQuantidadeAssinaturasAdicionaisSenadores())
+                .toArray();
+    }
+
     int getQuantidadeAssinaturasAdicionaisDeputados();
+
+    @SuppressWarnings("unused")
+    default int[] getAssinaturasAdicionaisDeputados() {
+        return IntStream.range(0, getQuantidadeAssinaturasAdicionaisDeputados())
+                .toArray();
+    }
 
     List<? extends Parlamentar> getParlamentares();
 
     @SuppressWarnings("unused")
-    default boolean hasOddNumberOfParlamentares() {
-        if (getParlamentares() == null) {
-            return false;
+    default List<Tuple2<? extends Parlamentar, ? extends Parlamentar>> getParlamentaresPair() {
+        final List<Tuple2<? extends Parlamentar, ? extends Parlamentar>> result = new ArrayList<>();
+        final int listSize = getParlamentares().size();
+        for (int i = 0; i < listSize; i += 2) {
+            if (listSize == i + 1) {
+                result.add(new Tuple2<>(getParlamentares().get(i), null));
+            } else {
+                result.add(new Tuple2<>(getParlamentares().get(i), getParlamentares().get(i + 1)));
+            }
+
         }
-
-        return getParlamentares().size() % 2 == 1;
-
+        return result;
     }
 
     ColegiadoAutor getColegiado();
