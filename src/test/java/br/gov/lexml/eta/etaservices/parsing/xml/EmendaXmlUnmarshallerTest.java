d6512911a986a6c05f042cf1d4a5f9a49ea7fd22
Marcos Fragomeni <marcos.fragomeni@gmail.com>
2022-08-31 12:30:57 -0300

    feat: extrai xml do pdf e converte p json

--- src/test/java/br/gov/lexml/eta/etaservices/parsing/xml/EmendaXmlUnmarshallerTest.java
+++ src/test/java/br/gov/lexml/eta/etaservices/parsing/xml/EmendaXmlUnmarshallerTest.java
@@ -9,13 +9,7 @@ import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-
+import br.gov.lexml.eta.etaservices.EmendaConverterService;
 import br.gov.lexml.eta.etaservices.emenda.Emenda;
 
 class EmendaXmlUnmarshallerTest {
@@ -38,20 +32,11 @@ class EmendaXmlUnmarshallerTest {
 
         assertThat(e.getLocal()).isEqualTo("Sala da comissão");
         
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-        
-        ObjectWriter objectWriter = objectMapper
-        		.setSerializationInclusion(Include.NON_NULL)
-        		.registerModule(new JavaTimeModule())
-        		.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)        		
-        		.writerWithDefaultPrettyPrinter()
-        		.forType(Emenda.class);
-        
         FileWriter fileWriter = new FileWriter("target/" + fileName + ".json");
-        JsonGenerator jsonGenerator = objectWriter.createGenerator(fileWriter);
-        jsonGenerator.writeObject(e);
-        jsonGenerator.flush();
+        
+        new EmendaConverterService().writeJson(e, fileWriter);
+        
         fileWriter.close();
+        
     }
 }
