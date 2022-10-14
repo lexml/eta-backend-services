17a621f8ef1f7c5d5be65aebd16aaf976e4a9928
Ricardo da Silva Lima <rslima@senado.leg.br>
2022-10-14 12:09:44 -0300

    fix: fecha arquivos no método extractJsonFromPdf

--- src/main/java/br/gov/lexml/eta/etaservices/emenda/EmendaJsonGeneratorBean.java
+++ src/main/java/br/gov/lexml/eta/etaservices/emenda/EmendaJsonGeneratorBean.java
@@ -1,25 +1,22 @@
 package br.gov.lexml.eta.etaservices.emenda;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.Writer;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
-import org.apache.commons.io.IOUtils;
-
+import br.gov.lexml.eta.etaservices.parsing.xml.EmendaXmlUnmarshaller;
+import br.gov.lexml.pdfa.PDFAttachmentHelper;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.apache.commons.io.IOUtils;
 
-import br.gov.lexml.eta.etaservices.parsing.xml.EmendaXmlUnmarshaller;
-import br.gov.lexml.pdfa.PDFAttachmentHelper;
+import java.io.File;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class EmendaJsonGeneratorBean implements EmendaJsonGenerator {
 	
@@ -29,18 +26,24 @@ public class EmendaJsonGeneratorBean implements EmendaJsonGenerator {
 		Path attachmentsDirPath = Files.createTempDirectory("extractJsonFromPdf");
 		
 		File pdfFile = new File(attachmentsDirPath.toFile(), "emenda.pdf");
-		
-		IOUtils.copy(pdfStream, new FileOutputStream(pdfFile));
+
+		try (OutputStream fos = Files.newOutputStream(pdfFile.toPath())) {
+			IOUtils.copy(pdfStream, fos);
+		}
 		
 		PDFAttachmentHelper.extractAttachments(pdfFile.getPath(), 
 				attachmentsDirPath.toAbsolutePath().toString());
 		
 		File xmlFile = new File(attachmentsDirPath.toFile(), "emenda.xml");
-		String xml = IOUtils.toString(new FileInputStream(xmlFile), StandardCharsets.UTF_8.name());
-		
-        Emenda e = new EmendaXmlUnmarshaller().fromXml(xml);
-		
-		writeJson(e, jsonWriter);
+
+		try (InputStream fin  = Files.newInputStream(xmlFile.toPath())) {
+
+			String xml = IOUtils.toString(fin, StandardCharsets.UTF_8);
+
+			Emenda e = new EmendaXmlUnmarshaller().fromXml(xml);
+
+			writeJson(e, jsonWriter);
+		}
 		
 	}
 
@@ -56,10 +59,12 @@ public class EmendaJsonGeneratorBean implements EmendaJsonGenerator {
         		.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)        		
         		.writerWithDefaultPrettyPrinter()
         		.forType(Emenda.class);
-        
-        JsonGenerator jsonGenerator = objectWriter.createGenerator(writer);
-        jsonGenerator.writeObject(e);
-        jsonGenerator.flush();
+
+
+		try(JsonGenerator jsonGenerator = objectWriter.createGenerator(writer)) {
+			jsonGenerator.writeObject(e);
+			jsonGenerator.flush();
+		}
         
 	}
 	
