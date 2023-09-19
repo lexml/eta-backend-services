c534841d95c118ff69913155550e88590238198d
Marcos Fragomeni <marcos.fragomeni@gmail.com>
2023-09-19 09:56:49 -0300

    feat: centraliza imagens no PDF
    refs #40

--- src/main/java/br/gov/lexml/eta/etaservices/printing/pdf/VelocityExtension.java
+++ src/main/java/br/gov/lexml/eta/etaservices/printing/pdf/VelocityExtension.java
@@ -61,6 +61,7 @@ public class VelocityExtension {
 		html = multipleReplaceAll(html, "<table .+?</table>", (m) -> m.group().replace("$pMarginBottom", "0"));
 		
 		String htmlAttrFo = html
+			.replaceAll("<p(.+?)><img", "<p$1 class=\"align-center\"><img")
 			.replaceAll("(class=\"[^\"]*)estilo-ementa", "margin-left=\"6.5cm\" text-indent=\"0\" $1")
 			.replaceAll("(class=\"[^\"]*)estilo-norma-alterada", "margin-left=\"3cm\" text-indent=\"1.5cm\" $1");
 		
